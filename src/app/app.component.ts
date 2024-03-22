601bcc4602d919c488aa02d8cd6e8b1f972ba98e
Pooja <poojacdr27@gmail.com>
2024-03-23 03:36:08 +0530

    Added home and about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    AboutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myportfolio';
+  listofbuttons: string[]=['Home', 'About', 'Skills', 'Languages', 'Projects', 'Education', 'Experience', 'Contact', 'Social Media'];
+  state: string='About'
+
+  constructor(){}
+  navigationCall(clickstate: string){
+    this.state=clickstate;
+    console.log(this.state);
+
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  listofbuttons: string[]=['Home', 'About', 'Skills', 'Languages', 'Projects', 'Education', 'Experience', 'Contact', 'Social Media'];
+  state: string='home'
 
+  constructor(){
+
+  }
 }
