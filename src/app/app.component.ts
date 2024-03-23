488de45570b675a17054dcbed322e739769bfe74
Pooja <poojacdr27@gmail.com>
2024-03-23 21:19:48 +0530

    Updating navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +17,14 @@ import { AboutComponent } from './components/about/about.component';
     CommonModule,
     RouterOutlet,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    SkillsComponent,
+    LanguagesComponent,
+    ProjectsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ContactComponent
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
